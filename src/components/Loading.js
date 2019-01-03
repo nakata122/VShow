8ef02e9ea2a471bc058ef481f9937b7b98f503c5
nakata122 <redspot122@gmail.com>
2019-01-03 18:30:06 +0200

    Changed server directory

--- src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loading = () => (
-    <div className='App-header'>
-        <h1>Loading...</h1>
-    </div>
-);
-
-export default Loading;
--- src/components/home/ExampleItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const LeaderboardItem = ({name,index}) => (
-    <div>{index+1}. {name}</div>
-);
-
-export default LeaderboardItem;
